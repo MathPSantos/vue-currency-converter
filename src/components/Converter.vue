b165d7725a77d2d6e8b7b55ac89cbc685f8550b4
MathPSantos <matheus.psantos2016@gmail.com>
2021-04-11 12:04:50 -0300

    intial commit

---
+++ src/components/Converter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="converter">
+    <div>
+      <div class="form-control">
+        <label for="you-have">You have</label>
+        <input id="you-have" type="text">
+      </div>
+
+      <div class="form-control">
+        <label for="you-have">You have</label>
+        <input id="you-have" type="text">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Converter'
+}
+</script>
+
+<style scoped lang="scss">
+  .converter {
+
+    border-radius: 0.5rem;
+
+    background: var(--white) 
+  }
+
+  .converter div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+
+  .converter div .form-control {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    padding: 2rem 4rem;
+  }
+</style>
